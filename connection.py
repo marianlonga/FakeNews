98799889e507532e07605d5a1b478dfe6564e23a
Marian Longa <marian.longa@gmail.com>
2017-08-04 17:10:43 +0100

    [ML] modify connection.py to get more metadata from DB, use this to add lots of base and derived features to plots.py

--- connection.py
+++ connection.py
@@ -1,8 +1,6 @@
 
 # coding: utf-8
 
-# In[4]:
-
 from pymongo import MongoClient
 import unicodecsv as csv
 
@@ -14,9 +12,6 @@ coll = db['tweets_onemonth']
 ## get all tweets in the DB
 alltweets = coll.find()
 print("ALL: " + str(coll.count())) ## total number of tweets in dataset
-
-
-# In[5]:
 
 ## indicate how to obtain the dataset
 pipeline = [
@@ -36,57 +31,86 @@ pipeline = [
         "num_hashtags" : {"$first": "$entities.hashtags"},
         "num_mentions" : {"$first": "$entities.user_mentions"},
         "num_urls" : {"$first": "$entities.urls"},
-        "num_media" : {"$first": "$entities.media"}
+        "num_media" : {"$first": "$entities.media"},
+
+        "user_default_profile_image" : {"$first": "$retweeted_status.user.default_profile_image"},
+        "user_description" : {"$first": "$retweeted_status.user.description"},
+        "user_listed_count" : {"$first": "$retweeted_status.user.listed_count"},
+        "user_name" : {"$first": "$retweeted_status.user.name"},
+        "user_profile_use_background_image" : {"$first": "$retweeted_status.user.profile_use_background_image"},
+        "user_default_profile" : {"$first": "$retweeted_status.user.default_profile"},
+        "user_statuses_count" : {"$first": "$retweeted_status.user.statuses_count"},
+        "user_created_at" : {"$first": "$retweeted_status.user.created_at"}
         }},
     {"$project": {
         "created_at":1,
         "retweet_count":1,
-        "text":1,
-        "user_screen_name":1,
+        "text": {"$ifNull": ["$text", " "]},
+        "user_screen_name": {"$ifNull": ["$user_screen_name", " "]},
         "user_verified":1,
         "user_friends_count":1,
         "user_followers_count":1,
         "user_favourites_count":1,
-        "source":1,
+        "source": {"$ifNull": ["$source", " "]},
         "coordinates":{"$size": { "$ifNull": ["$num_hashtags", [] ] }},
         "num_hashtags" : {"$size": { "$ifNull": ["$num_hashtags", [] ] }},
         "num_mentions" : {"$size": { "$ifNull": ["$num_mentions", [] ] }},
         "num_urls" : {"$size": { "$ifNull": ["$num_urls", [] ] }},
-        "num_media" : {"$size": { "$ifNull": ["$num_media", [] ] }}
+        "num_media" : {"$size": { "$ifNull": ["$num_media", [] ] }},
+
+        "user_default_profile_image" : 1,
+        "user_description" : {"$ifNull": ["$user_description", " "]},
+        "user_listed_count" : 1,
+        "user_name" : {"$ifNull": ["$user_name", " "]},
+        "user_profile_use_background_image" : 1,
+        "user_default_profile" : 1,
+        "user_statuses_count" : 1,
+        "user_created_at" : 1
         }},
     {"$sort": {"_id": 1}}
 ]
 
-
-# In[6]:
-
 ## extract our dataset
 viralTweets = coll.aggregate(pipeline)
+
 ## write it to a file
-with open("dataset_2.csv", "wb") as f:
+with open("dataset_new.tsv", "wb") as f:
     w = csv.writer(f, delimiter='\t', quotechar='"', encoding='utf-8')
-    w.writerow(['tweet_id','created_at','retweet_count','text','user_screen_name','user_verified','user_friends_count','user_followers_count','user_favourites_count', 'tweet_source', 'geo_coordinates','num_hashtags','num_mentions','num_urls','num_media'])
+    w.writerow(['tweet_id','created_at','retweet_count','text','user_screen_name','user_verified','user_friends_count',
+                'user_followers_count','user_favourites_count', 'tweet_source', 'geo_coordinates','num_hashtags',
+                'num_mentions','num_urls','num_media',     'user_default_profile_image','user_description',
+                'user_listed_count','user_name', 'user_profile_use_background_image', 'user_default_profile',
+                'user_statuses_count', 'user_created_at'])
     for d in viralTweets:
         w.writerow([
                 d['_id'],
                 d['created_at'],
                 d['retweet_count'],
                 d['text'].replace('\t',' ').replace('\n',' ').replace('\"','\''),
-                d['user_screen_name'],
+                d['user_screen_name'].replace('\t',' ').replace('\n',' ').replace('\"','\''),
                 d['user_verified'],
                 d['user_friends_count'],
                 d['user_followers_count'],
                 d['user_favourites_count'],
-                d['source'],
+                d['source'].replace('\t',' ').replace('\n',' ').replace('\"','\''),
                 d['coordinates'],
                 d['num_hashtags'],
                 d['num_mentions'],
                 d['num_urls'],
-                d['num_media']
+                d['num_media'],
+
+                d['user_default_profile_image'],
+                d['user_description'].replace('\t',' ').replace('\n',' ').replace('\"','\''),
+                d['user_listed_count'],
+                d['user_name'].replace('\t',' ').replace('\n',' ').replace('\"','\''),
+                d['user_profile_use_background_image'],
+                d['user_default_profile'],
+                d['user_statuses_count'],
+                d['user_created_at']
         ])
 
 
-# In[ ]:
+print("Done")
 
 
 
--- plots.py
+++ plots.py
@@ -1,20 +1,26 @@
 #TODO: change histogram to bar chart
 #TODO: add more derived features
 
+#TODO: add derived features for 'user_description', 'user_name', etc features
+#TODO: add feature for extracted year from 'user_created_at'
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
 import seaborn as sns
 import re
+import datetime
 
 sns.set(color_codes=True)
 
-INPUT_FILE_NAME = 'twitter_data_OneMonth_20170731.csv'
+#INPUT_FILE_NAME = 'twitter_data_OneMonth_20170731.csv'
+INPUT_FILE_NAME = 'dataset_new_combined_20170804.tsv'
 PLOTS_PATH = 'plots/'
+STATISTICS_FILE_NAME = 'statistics.txt'
 
 # read csv file
-data = pd.read_csv(INPUT_FILE_NAME)
+data = pd.read_csv(INPUT_FILE_NAME, sep='\t')
 data = data.dropna(subset=['is_fake_news_2'])
 data = data[data['is_fake_news_2'] != 'UNKNOWN']
 data['fake'] = (data['is_fake_news_2'] == 'TRUE').astype(int)
@@ -33,7 +39,11 @@ def extract_weekday_number(text):
     weekdays = {'Mon': 1, 'Tue': 2, 'Wed': 3, 'Thu': 4, 'Fri': 5, 'Sat': 6, 'Sun': 7}
     weekday = re.search('^([A-Z][a-z][a-z])\s', text).group(1)
     return weekdays[weekday]
-
+def get_user_created_at_delta(datetime_created_string):
+    datetime_today = datetime.datetime.now()
+    datetime_created = datetime.datetime.strptime(datetime_created_string, "%a %b %d %H:%M:%S +0000 %Y")
+    datetime_delta = datetime_today - datetime_created
+    return datetime_delta.days
 data['user_screen_name_number_of_digits']                           = data['user_screen_name'].apply(lambda text:     len(re.findall('[0-9]',     text)))
 data['user_screen_name_has_digits']                                 = data['user_screen_name'].apply(lambda text: int(len(re.findall('[0-9]',     text)) >= 1))
 data['user_screen_name_number_of_caps']                             = data['user_screen_name'].apply(lambda text:     len(re.findall('[A-Z]',     text)))
@@ -52,6 +62,19 @@ data['created_at_hour_23_to_5']                                     = data['crea
 data['created_at_hour_13_to_22']                                    = data['created_at_hour'].isin(range(13, 23))
 data['created_at_weekday']                                          = data['created_at'].apply(extract_weekday_number)
 data['created_at_weekday_sun_mon_tue']                              = data['created_at_weekday'].isin([7, 1, 2])
+data['user_description_has_special_chars']                          = data['user_description'].apply(lambda text: int(len(re.findall('[A-Z0-9]',  text)) >= 1))
+data['user_name_has_digits_underscore']                             = data['user_name'].apply(lambda text: int(len(re.findall('[0-9]_',     text)) >= 1))
+data['user_created_at_delta']                                       = data['user_created_at'].apply(get_user_created_at_delta)
+data['user_statuses_count_per_day']                                 = data['user_statuses_count'] / data['user_created_at_delta']
+data['user_name_has_weird_chars']                                   = data['user_name'].apply(lambda text: int(len(re.findall('[^A-Za-z .\']',     text)) >= 1)) # matches chars which should not be in a name
+data['user_name_number_weird_chars']                                = data['user_name'].apply(lambda text: len(re.findall('[^A-Za-z .\']',     text))) # matches chars which should not be in a name
+data['user_name_has_nonprintable_chars']                            = data['user_name'].apply(lambda text: int(len(re.findall('[^ -~]',     text)) >= 1)) # matches chars which should not be in a name
+data['user_name_number_nonprintable_chars']                         = data['user_name'].apply(lambda text: len(re.findall('[^ -~]',     text)) >= 1) # matches chars which should not be in a name
+data['user_description_has_weird_chars']                            = data['user_description'].apply(lambda text: int(len(re.findall('[^A-Za-z0-9., @#&]',  text)) >= 1))
+data['user_description_number_weird_chars']                         = data['user_description'].apply(lambda text: len(re.findall('[^A-Za-z0-9., @#&]',  text)))
+data['user_description_number_of_caps']                             = data['user_description'].apply(lambda text:     len(re.findall('[A-Z]',     text)))
+data['user_name_number_of_caps']                                    = data['user_name'].apply(lambda text:     len(re.findall('[A-Z]',     text)))
+
 
 # divide data set into fake and other news
 data_fake = data[data['fake'] == 1]
@@ -59,18 +82,26 @@ data_other = data[data['fake'] == 0]
 
 # plot continuous features
 continuous_features = ['retweet_count', 'user_friends_count', 'user_followers_count',
-                       'user_favourites_count']
+                       'user_favourites_count', 'user_listed_count', 'user_statuses_count', 'user_created_at_delta',
+                       'user_statuses_count_per_day']
 for feature in continuous_features:
     fig, ax = plt.subplots()
     sns.kdeplot(np.log10(data_fake[feature][data_fake[feature] != 0]), shade=True, ax=ax)
     sns.kdeplot(np.log10(data_other[feature][data_other[feature] != 0]), shade=True, ax=ax)
     plt.savefig(PLOTS_PATH + feature + '.png')
+    plt.close()
+print("Continuous features plotted successfully")
 
 # plot discrete features
-discrete_features = ['user_verified', 'geo_coordinates_available', 'num_hashtags', 'num_mentions',
+discrete_features = ['user_verified', 'geo_coordinates', 'num_hashtags', 'num_mentions',
                      'num_urls', 'num_media', 'user_screen_name_has_special_chars', 'text_number_of_exclam',
                      'created_at_hour', 'created_at_hour_23_to_5', 'created_at_hour_13_to_22', 'created_at_weekday',
-                     'created_at_weekday_sun_mon_tue']
+                     'created_at_weekday_sun_mon_tue', 'user_default_profile_image', 'user_description_has_special_chars',
+                     'user_name_has_digits_underscore', 'user_profile_use_background_image', 'user_default_profile',
+                     'user_name_has_weird_chars', 'user_name_number_weird_chars', 'user_name_has_nonprintable_chars',
+                     'user_name_number_nonprintable_chars', 'user_description_has_weird_chars',
+                     'user_description_number_weird_chars', 'user_description_number_of_caps',
+                     'user_name_number_of_caps']
 weights_fake = np.ones(data_fake.shape[0])/len(data_fake)
 weights_other = np.ones(data_other.shape[0])/len(data_other)
 weights = [weights_fake, weights_other]
@@ -82,16 +113,24 @@ for feature in discrete_features:
     plt.ylabel("normalised density of tweets")
     plt.title("Distribution of tweets by feature '" + feature + "'")
     plt.savefig(PLOTS_PATH + feature + '.png')
+    plt.close()
+print("Discrete features plotted successfully")
 
 # calculate statistics
 statistics_features = ['tweet_id', 'retweet_count', 'user_verified', 'user_friends_count', 'user_followers_count',
-            'user_favourites_count', 'geo_coordinates_available', 'num_hashtags', 'num_mentions', 'num_urls',
+            'user_favourites_count', 'geo_coordinates', 'num_hashtags', 'num_mentions', 'num_urls',
             'num_media', 'user_screen_name_number_of_digits', 'user_screen_name_has_digits',
             'user_screen_name_number_of_caps', 'user_screen_name_has_caps', 'user_screen_name_number_of_special_chars',
             'user_screen_name_has_special_chars', 'user_screen_name_number_of_special_chars_with_underscore',
             'user_screen_name_has_special_chars_with_underscore', 'text_number_of_caps', 'text_has_caps',
             'text_number_of_exclam', 'text_has_exclam', 'text_number_of_swears', 'created_at_hour',
-            'created_at_hour_23_to_5', 'created_at_hour_13_to_22', 'created_at_weekday', 'created_at_weekday_sun_mon_tue']
+            'created_at_hour_23_to_5', 'created_at_hour_13_to_22', 'created_at_weekday', 'created_at_weekday_sun_mon_tue',
+            'user_default_profile_image', 'user_description_has_special_chars', 'user_listed_count',
+            'user_name_has_digits_underscore', 'user_profile_use_background_image', 'user_default_profile',
+            'user_statuses_count', 'user_created_at_delta', 'user_statuses_count_per_day', 'user_name_has_weird_chars',
+            'user_name_number_weird_chars', 'user_name_has_nonprintable_chars', 'user_name_number_nonprintable_chars',
+            'user_description_has_weird_chars', 'user_description_number_weird_chars', 'user_description_number_of_caps',
+            'user_name_number_of_caps']
 statistics = []
 for feature in statistics_features:
     t, p = stats.ttest_ind(data_fake[feature].values, data_other[feature].values)
@@ -99,8 +138,11 @@ for feature in statistics_features:
 statistics_sorted = sorted(statistics, key=lambda s: s[2])
 
 # print statistics
+statistics_file = open(STATISTICS_FILE_NAME, 'w')
 template = '{0:60} {1:30} {2:30}'
-print(template.format("FEATURE", "T VALUE", "P VALUE"))
+statistics_file.write(template.format("FEATURE", "T VALUE", "P VALUE"))
+statistics_file.write("\n")
 for statistic in statistics_sorted:
-    print(template.format(*statistic))
-
+    statistics_file.write(template.format(*statistic) + "\n")
+statistics_file.close()
+print("Statistics written successfully")
